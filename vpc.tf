d455765510426ea13e9c83f96d6099c1beba3b49
mit <mit945.mc@gmail.com>
2020-05-12 20:21:26 -0700

    edit vpc

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,11 @@
 resource "aws_vpc" "core-infra" {
   cidr_block = "10.0.0.0/16"
   instance_tenancy = "dedicated"
-
   tags = {
     Name = "core-infra"
  }
+}
+resource "aws_subnet" "core-infra" {
+  vpc_id = "${aws_vpc.core-infra.id}"
+  cidr_block = "10.0.0.0/24"
 }
