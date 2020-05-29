db1748a339848ecac734534e326a96063c39e4bd
mit <mit945.mc@gmail.com>
2020-05-28 17:21:44 -0700

    whitespace

--- vpc.tf
+++ vpc.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "core-infra" {
   cidr_block = "10.0.0.0/16"
   instance_tenancy = "dedicated"
   tags = {
-    Name = "core-infra"
+     Name = "core-infra"
   }
 }       
 resource "aws_subnet" "core-infra" {
