2a39945cd216ef5c11dbabb62c36a2f06f0ab7d5
mit <mit945.mc@gmail.com>
2020-05-28 18:50:41 -0700

    fix config 2

--- instance.tf
+++ instance.tf
@@ -1,6 +1,5 @@
 data "aws_ami" "amzn-ami" {
 	most_recent = true
-	instance_id = data.aws_ami.amzn-ami.id
 	filter{
 	  name = "name"
 	  values = ["amzn2-ami-hvm-*-gp2"]
@@ -8,4 +7,14 @@ data "aws_ami" "amzn-ami" {
 	
 	owners = ["137112412989"] #AWS
 
+}
+
+data "aws_instance" "core-infra" {
+  	instance_id = data.aws_ami.amzn-ami.id
+}
+
+resource "aws_instance" "web" {
+  	ami = data.aws_ami.amzn-ami.id
+ 	 instance_type = "t3.medium"
+ 
 }
