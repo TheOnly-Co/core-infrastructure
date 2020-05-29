f367a05fa2f8746f44672c6f10b90ad8208bee22
mit <mit945.mc@gmail.com>
2020-05-28 22:25:06 -0700

    security group

--- instance.tf
+++ instance.tf
@@ -11,14 +11,13 @@ data "aws_ami" "amzn-ami" {
 resource "aws_security_group" "allow_tls" {
 	name = "allow_tls"
 	description = "Allow TLS inbound traffic"
-        vpc_id = "${aws_vpc.core-infra.id}"
+        vpc_id = aws_vpc.core-infra.id
 
 	ingress {
 	  description = "TLS from VPC"
 	  from_port = 80
 	  to_port = 22
 	  protocol = "tcp"
-	  cidr_blocks = aws_vpc.core-infra.cidr_block 	
 	}
 
 	egress {
