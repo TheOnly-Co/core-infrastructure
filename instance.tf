b4ffd57c39932a5fd64469e861a3fc8d16ef75b1
mit <mit945.mc@gmail.com>
2020-05-29 22:50:47 -0700

    fix decr 3

--- instance.tf
+++ instance.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "allow_ssh" {
     vpc_id = aws_vpc.core-infra.id
 
     ingress {
-      description = "ssh with tcp"
+      description = "Allow SSH Connection"
       from_port = 22
       to_port = 22
       protocol = "tcp"
