9f83170b4c943c4fd849564943015e9e157da9d6
mit <mit945.mc@gmail.com>
2020-05-30 00:04:13 -0700

    adding aws route

--- instance.tf
+++ instance.tf
@@ -62,3 +62,15 @@ resource "aws_eip" "bastion-eip"{
 variable authorized_key {
     description = "The public ssh rsa key you generated"
 }
+
+resource "aws_route" "public_internet_gateway" {
+    count = var.create_vpc && length(var.public_subnets) > 0 ? 1 : 0
+    route_table_id = aws_route_table.public[0].id
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this[0].id
+    
+    timeouts {
+        create = "5m"
+    }
+}
+
