aa78a2e852b288f0263dbc802d3f1f5f75c77fa4
mit <mit945.mc@gmail.com>
2020-05-30 21:38:33 -0700

    change cidr

--- instance.tf
+++ instance.tf
@@ -76,7 +76,7 @@ resource "aws_route_table" "rt" {
     vpc_id = aws_vpc.core-infra.id
     
     route {
-        cidr_block = "10.0.0.0/16"
+        cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.gw.id
     }
 }
