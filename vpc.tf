90ee01ac26afc61667539d31b39eca92c63329f6
mit <mit945.mc@gmail.com>
2020-05-31 00:51:41 -0700

    change

--- vpc.tf
+++ vpc.tf
@@ -27,7 +27,7 @@ resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.core-infra.id
   
   route {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "10.0.1.0/24"
     gateway_id = aws_internet_gateway.gw.id
   }
  }
