93c2530122d0e13cd902b6eea168f9137d775597
mit <mit945.mc@gmail.com>
2020-05-31 11:26:55 -0700

    deleteaws route

--- vpc.tf
+++ vpc.tf
@@ -14,15 +14,6 @@ resource "aws_internet_gateway" "gw"{
   vpc_id = aws_vpc.core-infra.id
 }
 
-resource "aws_route" "public_internet_gateway" {
-  route_table_id = aws_route_table.rt.id #from the table below
-  instance_id = aws_instance.bastion-host.id
-  destination_cidr_block = "0.0.0.0/0"
-  timeouts {
-    create = "5m"
-  }
-}
-  
 resource "aws_route_table" "rt" {
   vpc_id = aws_vpc.core-infra.id
   
