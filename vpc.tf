b1597f3cbb7055ae00dca03b9b90d031c7751d88
mit <mit945.mc@gmail.com>
2020-05-31 11:37:08 -0700

    change name

--- vpc.tf
+++ vpc.tf
@@ -14,11 +14,11 @@ resource "aws_internet_gateway" "gw"{
   vpc_id = aws_vpc.core-infra.id
 }
 
-resource "aws_route_table" "rt" {
+resource "aws_route_table" "route-table" {
   vpc_id = aws_vpc.core-infra.id
   
   route {
-    cidr_block = "172.31.0.0/16"
+    cidr_block = "10.0.0.0/16"
     gateway_id = aws_internet_gateway.gw.id
   }
  }
