37a54502aa06ea9d19c2b5fb35cc131c898f8933
mit <mit945.mc@gmail.com>
2020-06-02 21:49:13 -0700

    add subnet

--- instance.tf
+++ instance.tf
@@ -40,16 +40,13 @@ resource "aws_key_pair" "infra-master" {
     public_key = var.authorized_key
 }
 
-variable "subnet_ids"  {
-    default = [aws_subnet.core-infra-a.id, aws_subnet.core-infra-b.id, aws_subnet.core-infra-c.id]
-}
 
 resource "aws_instance" "bastion-host" {
     ami = data.aws_ami.amzn-ami.id
     instance_type = "t3.medium"
     # count = 0
     security_groups = [aws_security_group.allow_ssh.id, aws_security_group.egress-all.id]
-    subnet_id  = "${element(var.subnetids, 1)}"
+    subnet_id  = aws_subnet.core-infra-a.id
     key_name = aws_key_pair.infra-master.id
 
 }
