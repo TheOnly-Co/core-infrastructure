f6a9af44c82d8c292304d3f08fdd54ad1c2312df
mit <mit945.mc@gmail.com>
2020-06-15 15:30:25 -0700

    add version

--- eks.tf
+++ eks.tf
@@ -17,6 +17,7 @@ provider "kubernetes" {
 module "core-infra-cluster" {
     source = "terraform-aws-module/eks/aws"
     cluster_name = "core-infra-cluster"
+    cluster_version = "1.16"
     subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"] 
     vpc_id = module.vpc-west.vpc_id
  
