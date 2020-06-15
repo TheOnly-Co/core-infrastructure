1a326a1bb0774ecef73b37f51c5a51c70f7a7956
mit <mit945.mc@gmail.com>
2020-06-15 15:54:51 -0700

    change naimg

--- eks.tf
+++ eks.tf
@@ -1,9 +1,9 @@
 data "aws_eks_cluster" "cluster" {
-    name = module.core-infra-cluster.cluster_id
+    name = module.core-infra-eks.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-    name = module.core-infra-cluster.cluster_id
+    name = module.core-infra-eks.cluster_id
 }
 
 provider "kubernetes" {
@@ -14,9 +14,9 @@ provider "kubernetes" {
     version = "~> 1.9"
 }
 
-module "core-infra-cluster" {
+module "core-infra-eks" {
     source = "terraform-aws-module/eks/aws"
-    cluster_name = "core-infra-cluster"
+    cluster_name = "core-infra-eks"
     cluster_version = "1.16"
     subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"] 
     vpc_id = module.vpc-west.vpc_id
