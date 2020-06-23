be1dcc23df69eddd8b403764fe5b42d492eff7d9
Keith H. Lin <keithlin.732@gmail.com>
2020-06-22 19:02:07 -0700

    added service role

---
+++ service_roles.tf
@@ -0,0 +1,25 @@
+source "aws_iam_role" "cicd-service-role" {
+  name               = "cicd-service-role"
+  assume_role_policy = data.aws_iam_policy_document.assume-service-role-policy.json
+}
+
+data "aws_iam_policy_document" "assume-service-role-policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      identifiers = ["ec2.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      identifiers = []
+      type        = "AWS"
+    }
+  }
+}
+
