53b5c49f40cc0c6927e947791f36d8adb0e70b4c
mit <mit945.mc@gmail.com>
2020-07-01 18:49:01 -0700

    use original

--- eks.tf
+++ eks.tf
@@ -26,12 +26,8 @@ module "core-infra-eks" {
     ]
     worker_groups = [
         {
-          name = "eks-spot"
-          spot_price = "0.05"
           instace_type = "t3.micro"
           asg_max_size = 1
-          kubelet_extra_args  = "--node-labels=node.kubernetes.io/lifecycle=spot"
-          suspended_processes = ["AZRebalance"]
         }
     
     ]
