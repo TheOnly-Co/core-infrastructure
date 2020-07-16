cface48abfd130c5438d8109e2a50b05ccdedb5d
mit <mit945.mc@gmail.com>
2020-07-16 14:14:23 -0700

    remove

--- eks.tf
+++ eks.tf
@@ -40,7 +40,7 @@ resource "aws_eks_node_group" "elastic" {
     cluster_name = "core-infra-eks"
     node_group_name = "elastic"
     node_role_arn = "arn:aws:iam::216228501626:role/core-infra-eks20200616045837471300000001" 
-    subnet_ids = "subnet-0964059ea7a5f1021"
+    subnet_ids = subnet-0964059ea7a5f1021
 
     scaling_config {
         desired_size = 4
