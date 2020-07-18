64e6f385153fed83bd74b01a6dceebef6ed0dfff
mit <mit945.mc@gmail.com>
2020-07-17 17:24:22 -0700

    cool

--- elasticsearch.tf
+++ elasticsearch.tf
@@ -1,5 +1,5 @@
-resource "aws_elasticsearch_domain" "core-infra_eks_logs" {
-    domain_name = "core-infra_eks_logs"
+resource "aws_elasticsearch_domain" "core-infra-eks-logs" {
+    domain_name = "core-infra-eks-logs"
     cluster_config {
       instance_type = "t2.small.elasticsearch"
     }
