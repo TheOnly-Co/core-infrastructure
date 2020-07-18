164ae7f74ccbc9d88a912474539e9220d8d4ad6e
mit <mit945.mc@gmail.com>
2020-07-17 21:46:09 -0700

    changing module

--- elasticsearch.tf
+++ elasticsearch.tf
@@ -1,7 +1,16 @@
-resource "aws_elasticsearch_domain" "core-infra-eks-logs" {
-    domain_name = "core-infra-eks-logs"
-    cluster_config {
-      instance_type = "t2.small.elasticsearch"
-    }
-    
+module "core-infra-es" {
+  source = "git::https://github.com/cloudposse/terraform-aws-elasticsearch.git?ref=master"
+  name = "core-infra-es"
+  vpc_id = module.vpc-west.vpc_id
+  subnet_ids = module.vpc-west.public_subnets
 }
+
+
+
+# resource "aws_elasticsearch_domain" "core-infra-eks-logs" {
+#     domain_name = "core-infra-eks-logs"
+#     cluster_config {
+#       instance_type = "t2.small.elasticsearch"
+#     }
+#     
+# }
